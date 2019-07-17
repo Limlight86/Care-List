8fb7996a89e9facfac97f9848cbb811a8cd1979e
Lazaro <lquin008@gmail.com>
2019-07-17 15:14:24 -0400

    initial alphabetize func, needs to go further

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -6,6 +6,21 @@ import styles from "./Layout.module.css";
 
 let i = 0;
 
+const alphabetize = (arr) =>{
+  arr.sort((a, b) => {
+    if (!a.text || !a.text[0] || !a.text[0] || !b.text || !b.text[0] || !b.text[0]) {
+      return 0;
+    } else if (a.text[0].toLowerCase() < b.text[0].toLowerCase()) {
+      return -1;
+    } else if (a.text[0].toLowerCase() > b.text[0].toLowerCase()) {
+      return 1;
+    } else {
+      return 0;
+    }
+  })
+  return arr
+}
+
 class Layout extends Component {
   state = {
     needToBuyList: [],
@@ -31,17 +46,7 @@ class Layout extends Component {
     }
 
     this.setState({
-      needToBuyList: [...this.state.needToBuyList, todo].sort((a, b) => {
-        if (!a.text || !a.text[0] || !a.text[0] || !b.text || !b.text[0] || !b.text[0]) {
-          return 0;
-        } else if (a.text[0].toLowerCase() < b.text[0].toLowerCase()) {
-          return -1;
-        } else if (a.text[0].toLowerCase() > b.text[0].toLowerCase()) {
-          return 1;
-        } else {
-          return 0;
-        }
-      })
+      needToBuyList: alphabetize([...this.state.needToBuyList, todo])
     });
     i++;
     e.target.elements.groceryItem.value = "";
@@ -66,18 +71,7 @@ class Layout extends Component {
       swappedItem[0].inCart = false;
       this.setState({
         inCartList,
-        needToBuyList: [...this.state.needToBuyList, swappedItem[0]].sort((a, b) => {
-            if (!a.text || !a.text[0] || !a.text[0] || !b.text || !b.text[0] || !b.text[0]) { 
-              return 0;
-            } else if (a.text[0].toLowerCase() < b.text[0].toLowerCase()) {
-              return -1;
-            } else if (a.text[0].toLowerCase() > b.text[0].toLowerCase()) {
-              return 1;
-            } else {
-              return 0;
-            }
-          }
-        )
+        needToBuyList: alphabetize([...this.state.needToBuyList, swappedItem[0]])
       });
     } else {
       needToBuyList = needToBuyList.filter(item => {
@@ -86,17 +80,7 @@ class Layout extends Component {
       swappedItem[0].inCart = true;
       this.setState({
         needToBuyList,
-        inCartList: [...this.state.inCartList, swappedItem[0]].sort((a, b) => {
-          if ( !a.text || !a.text[0] || !a.text[0] || !b.text || !b.text[0] || !b.text[0]) {
-            return 0;
-          } else if (a.text[0].toLowerCase() < b.text[0].toLowerCase()) {
-            return -1;
-          } else if (a.text[0].toLowerCase() > b.text[0].toLowerCase()) {
-            return 1;
-          } else {
-            return 0;
-          }
-        })
+        inCartList: alphabetize([...this.state.inCartList, swappedItem[0]])
       });
     }
   };
