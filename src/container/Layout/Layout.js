9af8dc2b14576c2f72fc960dded45016e0cb5ab6
Lazaro <lquin008@gmail.com>
2019-07-16 23:46:34 -0400

    input functionality added

--- src/container/Layout/Layout.js
+++ src/container/Layout/Layout.js
@@ -1,10 +1,39 @@
-import React from "react";
-import styles from './Layout.module.css'
+import React, { Component } from "react";
+import ShoppingList from "../../components/ShoppingList/ShoppingList";
+import ShoppingInput from "../../components/ShoppingInput/ShoppingInput";
+import ListsBody from "../ListsBody/ListsBody";
+import styles from "./Layout.module.css";
 
-const layout = props => (
-  <div className={styles.layout}>
-    {props.children}
-  </div>
-)
+class Layout extends Component {
+  state = {
+    needToBuyList: [],
+    inCartList: []
+  };
 
-export default layout
+  handleSubmit = e => {
+    e.preventDefault();
+    let text = e.target.elements.groceryItem.value;
+    let todo = {text: text, inCart : false}
+    if (!text.trim()){
+      e.target.elements.groceryItem.value = "";
+      return;
+    }
+    this.setState({needToBuyList : [...this.state.needToBuyList, todo]});
+    e.target.elements.groceryItem.value = "";
+  };
+
+  render() {
+    const {needToBuyList, inCartList} = this.state
+    return (
+      <div className={styles.layout}>
+        <h1>Care-List</h1>
+        <ShoppingInput addToList={this.handleSubmit} />
+        <ListsBody>
+          <ShoppingList listName={"Need to Buy"} list={needToBuyList} />
+          <ShoppingList listName={"In My Cart"} list={inCartList} />
+        </ListsBody>
+      </div>
+    );
+  }
+}
+export default Layout;
