69d541953069959263d98748a09c2bb7e1c5a10e
Lazaro <lquin008@gmail.com>
2019-07-20 22:27:32 -0400

    refactored code

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const axios = require("axios");
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 8000;
 
-app.get('/api', (req, res) => {
-  res.send('YOUR EXPRESS BACKEND IS CONNECTED TO REACT');
-})
+let groceryList = {
+  needToBuyList: [],
+  inCartList: []
+};
+
+app.get("/api", (req, res) => {
+  res.send(groceryList);
+});
+
+app.post("/api", (req, res) => {
+  groceryList = req.body;
+  res.end();
+});
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
-
